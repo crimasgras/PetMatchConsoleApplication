0b32824695e710ec8173a22867f8a0647f430732
Crina Masgras <crinamasgras@yahoo.com>
2026-03-15 02:00:05 +0200

    Update database password in DataBaseConnection

--- src/DataBaseConnection.java
+++ src/DataBaseConnection.java
@@ -6,7 +6,7 @@ public class DataBaseConnection {
 
     private static final String URL      = "jdbc:postgresql://localhost:5432/pet_adoption";
     private static final String USER     = "postgres";
-    private static final String PASSWORD = "r24c20!!";
+    private static final String PASSWORD = "s23c78!!";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
