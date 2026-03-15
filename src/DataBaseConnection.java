999eccd58eec0a37092f395e19171eafeb7d37c8
Crina Masgras <crinamasgras@yahoo.com>
2026-03-15 02:05:30 +0200

    Add DataBaseConnection class for PostgreSQL connection

---
+++ src/DataBaseConnection.java
@@ -0,0 +1,14 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DataBaseConnection {
+
+    private static final String URL      = "jdbc:postgresql://localhost:5432/pet_adoption";
+    private static final String USER     = "postgres";
+    private static final String PASSWORD = "s23c78!!!";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
